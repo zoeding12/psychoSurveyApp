549b5404571ff8e8063d8d5e7f49042884e2cf5b
changrong <changrongnj@gmail.com>
2020-07-24 22:50:53 -0700

    springboot template

---
+++ demo/src/main/java/njbh/autism/demo/PsychoApplication.java
@@ -0,0 +1,13 @@
+package njbh.autism.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsychoApplication.class, args);
+	}
+
+}
