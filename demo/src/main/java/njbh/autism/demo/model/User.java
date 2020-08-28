03a621d2b3b5c412ca18e5d35fa76d64ef9b8d88
changrong <changrongnj@gmail.com>
2020-08-28 14:44:58 -0700

    basic post/get setup

--- demo/src/main/java/njbh/autism/demo/model/User.java
+++ demo/src/main/java/njbh/autism/demo/model/User.java
@@ -1,53 +1,43 @@
 package njbh.autism.demo.model;
 
 import javax.persistence.*;
-import java.sql.Date;
-import java.util.UUID;
 
 @Entity // this tells Hibernate to make a table out of this class
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private UUID id;
+    @GeneratedValue
+    private int id;
     private String name;
-    private String birthdate;
-    private int age;
+    private String birthday;
     private String email;
 
-    public UUID getId() {
-        return id;
+    protected User() {}
+
+    public User(int id, String name, String birthday, String email) {
+        this.id = id;
+        this.name = name;
+        this.birthday = birthday;
+//        this.age = age;
+        this.email = email;
     }
 
-    public void setId(UUID id) {
-        this.id = id;
+    public int getId() {
+        return id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getBirthday() {
+        return birthday;
     }
 
-    public String getDate() {
-        return birthdate;
-    }
-
-    public void setDate(Date date) {
-        this.birthdate = birthdate;
-    }
-
-    public int getAge() { return age; }
-
-    public void setAge(int age) { this.age = age; }
+//    public int getAge() { return age; }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
--- demo/src/main/java/njbh/autism/demo/repository/UserRepository.java
+++ demo/src/main/java/njbh/autism/demo/repository/UserRepository.java
@@ -1,8 +1,13 @@
 package njbh.autism.demo.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.*;
 import njbh.autism.demo.model.User;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
+@Component("UserRepository")
+@Repository
 public interface UserRepository extends CrudRepository<User, String> {
 
+    User findAllByName(String name);
 }
